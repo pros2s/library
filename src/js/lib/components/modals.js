563ac3fc785888237fe15601963631ead0e96501
Alexandr <caiiiia_podoplelov@mail.ru>
2022-03-25 19:47:07 +0300

    dynamic creation modals

--- src/js/lib/components/modals.js
+++ src/js/lib/components/modals.js
@@ -1,29 +1,81 @@
 import $ from '../core';
 
+const ANIMATIONTIME = 300;
 
-$.prototype.modal = function() {
+$.prototype.modal = function(created) {
+
   for (let i = 0; i < this.length; i++) {
     const target = $(this[i]).attribute('data-target');
+
     $(this[i]).click((e) => {
       e.preventDefault();
-      $(target).fadeIn(300);
+      $(target).fadeIn(ANIMATIONTIME);
       document.body.style.overflow = 'hidden';
     });
 
     document.querySelectorAll(`${target} [data-close]`).forEach((btn) => {
       $(btn).click(() => {
-        $(target).fadeOut(300);
+        $(target).fadeOut(ANIMATIONTIME);
         document.body.style.overflow = '';
       });
     });
 
     $(target).click((e) => {
       if (e.target.classList.contains('modal')) {
-        $(target).fadeOut(300);
+        $(target).fadeOut(ANIMATIONTIME);
         document.body.style.overflow = '';
+
+        setTimeout(() => {
+          if (created) document.querySelector(target).remove();
+        }, ANIMATIONTIME);
       }
     });
   }
 };
 
 $('[data-toggle="modal"]').modal();
+
+
+$.prototype.createModal = function({inner, btns} = {}) {
+  // eslint-disable-next-line
+  const {count, settings} = btns;// btns params
+
+  for (let i = 0; i < this.length; i++) {
+    let modalElement = document.createElement('div');
+    $(modalElement).addClass('modal');
+    $(modalElement).addAttribute('id', $(this[i]).attribute('data-target').slice(1));
+    $(modalElement).html(`
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <button class="close" data-close><span>&times;</span></button>
+        <div class="modal-header">
+          <h3 class="modal-title">${inner.title}</h3>
+        </div>
+        <div class="modal-body p20">${inner.body}</div>
+        <div class="modal-footer">
+
+        </div>
+      </div>
+    </div>`);
+
+    const buttons = [];
+    for (let j = 0; j < count; j++) {
+      const button = document.createElement('button');
+      $(button).addClass('btn', ...settings[j].classes);
+      $(button).html(settings[j].content);
+
+      if (settings[j].callback && typeof settings[j].callback === 'function') {
+        $(button).click(settings[j].callback);
+      }
+
+      if (settings[j].close) $(button).addAttribute('data-close');
+
+      buttons.push(button);
+    }
+
+    modalElement.querySelector('.modal-footer').append(...buttons);
+    document.body.appendChild(modalElement);
+    $(this[i]).modal(true);
+    $(this[i].getAttribute('data-target')).fadeIn(ANIMATIONTIME);
+  }
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
 import './lib/lib';
+
+$('#modalTrigger').click(() => $('#modalTrigger').createModal(
+  {
+    inner: {
+      title: 'First Modal',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam labore impedit nobis voluptatem ea veritatis numquam at molestiae ex rem dolor quam perspiciatis quidem assumenda, vero provident quasi quis voluptatum magni. Harum a'
+    },
+    btns: {
+      count: 3,
+      settings: [
+        {
+          classes: ['btn-danger', 'mr-10'],
+          content: 'close',
+          close: true
+        },
+        {
+          classes: ['btn-success', 'mr-10'],
+          content: 'save',
+          callback: () => alert('Saved')
+        },
+        {
+          classes: ['btn-warning'],
+          content: 'warning',
+          callback: () => {
+            alert('warning');
+            console.log('warning');
+          }
+        }
+      ]
+    }
+  }
+));
